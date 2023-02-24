02f748ebcaa69b7cec67bf19d5cd6fd81741b973
Seungjin Kang <xeungjin@gmail.com>
2023-02-24 12:59:45 +0900

    (~p476)

--- web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
+++ web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -1,5 +1,11 @@
 package com.saeyan.dao;
 
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class MemberDAO {
 	private MemberDAO() {
 
@@ -9,5 +15,14 @@ public class MemberDAO {
 
 	public static MemberDAO getInstance() {
 		return instance;
+	}
+
+	public Connection getConnection() throws Exception {
+		Connection conn = null;
+		Context initContext = new InitialContext();
+		Context envContext = (Context) initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+		conn = ds.getConnection();
+		return conn;
 	}
 }

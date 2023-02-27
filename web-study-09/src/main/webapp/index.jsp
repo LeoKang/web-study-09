ab4f6cafeda283b93ec08f588eb00e9169b0cae3
Seungjin Kang <xeungjin@gmail.com>
2023-02-27 11:16:24 +0900

    ~494page

--- web-study-09/src/main/webapp/index.jsp
+++ web-study-09/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,6 @@
 <title>회원 관리</title>
 </head>
 <body>
-<a href="login.do">로그인 페이지로 이동</a>
+	<a href="login.do">로그인 페이지로 이동</a>
 </body>
 </html>
--- web-study-09/src/main/webapp/script/member.js
+++ web-study-09/src/main/webapp/script/member.js
@@ -14,3 +14,14 @@
 	}
 	return true;
 }
+
+function idCheck() {
+	if(document.frm.userid.value=="") {
+		alert('아이디를 입력하여 주십시오.');
+		document.frm.userid.focus();
+		return;
+	}	
+	var url="idCheck.do?userid="+ document.frm.userid.value;
+	window.open(url, "_blank_1", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=200");
+}
+
